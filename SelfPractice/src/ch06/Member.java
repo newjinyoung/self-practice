f3f047a3d3315e94423b38a41ef78b2edec64dfa
DESKTOP-C7M8I5D\my PC <jy930122@nate.com>
2023-01-15 19:30:56 +0900

    added ch06

---
+++ SelfPractice/src/ch06/Member.java
@@ -0,0 +1,14 @@
+package ch06;
+
+public class Member {
+
+	private String name;
+	private String id;
+	private String password;
+	private int age;
+	
+	Member(String name, String id){
+		this.name = name;
+		this.id = id;
+	}		
+}
