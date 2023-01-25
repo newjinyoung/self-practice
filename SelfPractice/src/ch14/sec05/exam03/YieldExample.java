c7c6302d2ad7cfd20b93984639b5893e40295b99
DESKTOP-C7M8I5D\my PC <jy930122@nate.com>
2023-01-26 00:00:20 +0900

    update

---
+++ SelfPractice/src/ch14/sec05/exam03/YieldExample.java
@@ -0,0 +1,18 @@
+package src.ch14.sec05.exam03;
+
+public class YieldExample {
+
+	public static void main(String[] args) {
+		WorkThread workThreadA = new WorkThread("workThreadA");
+		WorkThread workThreadB = new WorkThread("workThreadB");
+		workThreadA.start();
+		workThreadB.start();
+		
+		try { Thread.sleep(500); } catch (InterruptedException e) {}
+		workThreadA.work = false;
+		
+		try { Thread.sleep(10000); } catch (InterruptedException e) {}
+		workThreadA.work = true;
+	}
+
+}
