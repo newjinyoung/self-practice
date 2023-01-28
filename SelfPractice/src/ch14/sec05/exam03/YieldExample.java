5bb8af1093fd4b2555e92290b5ef85643935dded
DESKTOP-C7M8I5D\my PC <jy930122@nate.com>
2023-01-28 22:57:47 +0900

    Thread

--- SelfPractice/src/ch14/sec05/exam03/YieldExample.java
+++ SelfPractice/src/ch14/sec05/exam03/YieldExample.java
@@ -8,11 +8,12 @@ public class YieldExample {
 		workThreadA.start();
 		workThreadB.start();
 		
-		try { Thread.sleep(500); } catch (InterruptedException e) {}
+		try { Thread.sleep(5000); } catch (InterruptedException e) {}
 		workThreadA.work = false;
 		
 		try { Thread.sleep(10000); } catch (InterruptedException e) {}
 		workThreadA.work = true;
 	}
-
 }
+
+//무한러닝 멈추는법???
