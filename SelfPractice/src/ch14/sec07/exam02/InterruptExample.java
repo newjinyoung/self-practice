6370ec28a16017a996fdeed24b98c7ee4d2702cb
DESKTOP-C7M8I5D\my PC <jy930122@nate.com>
2023-01-29 17:36:22 +0900

    UPDATE

---
+++ SelfPractice/src/ch14/sec07/exam02/InterruptExample.java
@@ -0,0 +1,15 @@
+package src.ch14.sec07.exam02;
+
+public class InterruptExample {
+	public static void ain(String[] args) {
+		Thread thread = new PrintThread();
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		thread.interrupt();
+	}
+} 
